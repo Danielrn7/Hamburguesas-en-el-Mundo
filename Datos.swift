2ad743c83f2e467d929ea4979840893812ac6756
Danielrn7 <31738492+Danielrn7@users.noreply.github.com>
2017-10-07 21:42:24 -0500

    Update Datos.swift

--- Datos.swift
+++ Datos.swift
@@ -10,47 +10,43 @@ import Foundation
 import UIKit
 
 class ColeccionDePaises {
-    let paises : [String] = [
-        "Argentina", "Chile", "Brasil", "Uruguay", "Bolivia",
-        "Paraguay", "Perú", "Ecuador", "Colombia", "Venezuela",
-        "Panamá", "Nicaragua", "Costa Rica", "Cuba", "Haití",
-        "México", "Guatemala", "República Dominicana", "Puerto Rico", "Jamaica"]
     
+    let paises = [  "Argentina", "Chile", "Brasil", "Uruguay", "Bolivia",
+                    "Paraguay", "Perú", "Ecuador", "Colombia", "Venezuela",
+                    "Panamá", "Nicaragua", "Costa Rica", "Cuba", "Haití",
+                    "México", "Guatemala", "República Dominicana", "Puerto Rico", "Jamaica" ]
     
-    func obtenPais( )->String {
-        let posicion = Int(arc4random()) % paises.count
-        return paises[posicion]
+    func obtenPais () -> String {
+        let paisR = Int( arc4random() )  % paises.count
+        return paises[paisR]
     }
+    
 }
 
 class ColeccionDeHamburguesas {
-    let hamburguesas : [String] = [
-        "Hambur 1", "Hambur 2", "Hambur 3", "Hambur 4", "Hambur 5",
-        "Hambur 6", "Hambur 7", "Hambur 8", "Hambur 9", "Hambur 10",
-        "Hambur 11", "Hambur 12", "Hambur 13", "Hambur 14", "Hambur 15",
-        "Hambur 16", "Hambur 17", "Hambur 18", "Hambur 19", "Hambur 20"]
-    
-    func obtenHamburguesa( )->String {
-        let posicion = Int(arc4random()) % hamburguesas.count
-        return hamburguesas[posicion]
+
+    let hamburguesas = ["Doble Queso", "Triple Carne", "Americana con Queso", "Texana", "Ahumada", "Doble Pollo", "Pollo Asado", "Estilo Japones", "Estilo Barbacoa","Vegetal", "Sin Sal", "Bajo en Grasa", "Estilo California", "Salvaje", "Doble Pepinillo", "Doble Carne", "Cebolla Caramelizada", "Picante Extra Hot", "Picante Suave", "Estilo Vaquero"]
+
+    func obtenHamburguesa() -> String {
+        let burgerR = Int( arc4random() )  % hamburguesas.count
+        return hamburguesas[burgerR]
     }
-    
 }
 
 struct Colores {
-    let colores = [UIColor(red : 210/255.0, green : 90/255.0, blue : 45/255.0, alpha : 1),
-                   UIColor(red : 40/255.0, green : 170/255.0, blue : 45/255.0, alpha : 1),
-                   UIColor(red : 3/255.0, green : 180/255.0, blue : 90/255.0, alpha : 1),
-                   UIColor(red : 210/255.0, green : 190/255.0, blue : 5/255.0, alpha : 1),
-                   UIColor(red : 120/255.0, green : 120/255.0, blue : 50/255.0, alpha : 1),
-                   UIColor(red : 130/255.0, green : 80/255.0, blue : 90/255.0, alpha : 1),
-                   UIColor(red : 130/255.0, green : 130/255.0, blue : 130/255.0, alpha : 1),
-                   UIColor(red : 3/255.0, green : 50/255.0, blue : 90/255.0, alpha : 1)]
     
-    func regresaColorAleatorio() -> UIColor {
-        let posicion = Int(arc4random()) % colores.count
-        
+    let colores = [ UIColor(red:39/255.0, green: 88/255.0, blue: 107/255.0, alpha: 1),
+                    UIColor(red:9/255.0, green: 47/255.0, blue: 62/255.0, alpha: 1),
+                    UIColor(red:41/255.0, green: 123/255.0, blue: 72/255.0, alpha: 1),
+                    UIColor(red:7/255.0, green: 72/255.0, blue: 31/255.0, alpha: 1),
+                    UIColor(red:170/255.0, green: 116/255.0, blue: 57/255.0, alpha: 1),
+                    UIColor(red:99/255.0, green: 56/255.0, blue: 10/255.0, alpha: 1),
+                    UIColor(red:170/255.0, green: 76/255.0, blue: 57/255.0, alpha: 1),
+                    UIColor(red:99/255.0, green: 25/255.0, blue: 10/255.0, alpha: 1)]
+    
+    func regresaColorAleatorio () -> UIColor {
+        let posicion = Int( arc4random() )  % colores.count
         return colores[posicion]
-        
     }
+    
 }
